dd701eb458808c1f82245cf5b3240c56563f5be2
FlyingHail <flyinghail@msn.com>
2017-04-17 21:30:38 +0800

    bootstrap.php rename to autoload.php

--- optimize.php
+++ optimize.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/bootstrap.php';
+require __DIR__ . '/autoload.php';
 
 Hail\Framework::init();
 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php
-require __DIR__ . '/../bootstrap.php';
+require __DIR__ . '/../autoload.php';
 
 Hail\Framework::app()->run();
