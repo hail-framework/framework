cce11181d8d7e90a4550c3e69712cf82c02f2cf6
FlyingHail <flyinghail@msn.com>
2018-01-29 20:07:21 +0800

    add js function support

--- src/Template/Processor/Vue/Syntax.php
+++ src/Template/Processor/Vue/Syntax.php
@@ -38,30 +38,71 @@ class Syntax
             return '';
         }
 
-        if ($expression[0] === '!') { // ! operator application
-            return '!(' . static::jsParse(\substr($expression, 1)) . ')';
+        if (\is_numeric($expression)) {
+            return $expression;
         }
 
-        if ($expression[0] === '\'') {
-            return \var_export(\substr($expression, 1, -1), true);
+        if ($expression[0] === '!') { // ! operator application
+            return '!(' . static::parse(\substr($expression, 1)) . ')';
+        }
+
+        if ($expression[0] === '\'' && $expression[-1] === '\'') {
+            return static::string(\substr($expression, 1, -1));
         }
 
         $parts = \explode('.', $expression);
 
-        return static::variableAccess($parts);
+        return static::variable($parts);
     }
 
-    protected static function variableAccess(array $parts): string
+    protected static function string(string $exp): string
+    {
+        return \var_export($exp, true);
+    }
+
+    protected static function variable(array $parts): string
     {
         $value = '';
-        foreach ($parts as $key) {
+        foreach ($parts as $part) {
             if ($value === '') {
-                $value .= "\$this->wrap(\${$key})";
+                $value .= "\$this->wrap(\${$part})";
             } else {
-                $value .= "->{$key}";
+                if (\preg_match('/^(\w+)\((.*)\)$/', $part, $matches)) {
+                    $args = '';
+                    if (!empty($matches[2])) {
+                        $args = static::explode(',', $matches[2]);
+                    }
+
+                    $value .= "->{$matches[1]}({$args})";
+                } else {
+                    $value .= "->{$part}";
+                }
             }
         }
 
         return $value;
+    }
+
+    protected static function explode(string $delimiter, string $string): string
+    {
+        $temp = '';
+        $args = [];
+        foreach (\explode($delimiter, $string) as $v) {
+            if ($temp !== '') {
+                if ($v[-1] === '\'') {
+                    $args[] = $temp . $delimiter . $v;
+                    $temp = '';
+                } else {
+                    $temp .= $v;
+                }
+            } elseif ($v[0] === '\'' && !(isset($v[1]) && $v[-1] === '\'')) {
+                $temp = $v;
+            } else {
+                $args[] = $v;
+            }
+        }
+        $args = \array_map('self::parse', $args);
+
+        return \implode($delimiter, $args);
     }
 }
