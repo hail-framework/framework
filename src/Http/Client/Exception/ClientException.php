16c03c88854a09f0f2c7644c8265ccd19130b5b2
FlyingHail <flyinghail@msn.com>
2019-04-06 20:19:40 +0800

    fix http client

--- src/Http/Client/Exception/ClientException.php
+++ src/Http/Client/Exception/ClientException.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace Hail\Http\Client\Exception;
 
-use Http\Client\Exception as HTTPlugException;
 use Psr\Http\Client\ClientExceptionInterface as PsrClientException;
 
 /**
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
-class ClientException extends \RuntimeException implements ExceptionInterface, HTTPlugException, PsrClientException
+class ClientException extends \RuntimeException implements ExceptionInterface, PsrClientException
 {
 }
